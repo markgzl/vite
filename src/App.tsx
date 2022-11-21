9a1343018f473660009456cd730570dedc175c3c
zilin <zilin.guo@avatr.com>
2022-11-21 14:33:11 +0800

    add husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import style from "./App.module.scss";
 
 function App() {
-  // const [count, setCount] = useState(0);
-  // console.log(count);
+  const [count, setCount] = useState(0);
+  console.log(count);
   return (
     <div className="app">
       <h1 className="h">Hello Vite + React!</h1>
